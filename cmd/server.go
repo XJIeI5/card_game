641b7c9e75e2bea4c778641a89246e14cb2700e2
Егор Чернов <egorchernov37@yandex.ru>
2023-12-22 22:53:39 +0300

    clean up server entery point

--- cmd/server.go
+++ cmd/server.go
@@ -1,39 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"net"
 
-	"github.com/XJIeI5/card_game/cmd/helper"
-	"github.com/XJIeI5/card_game/pkg/socket/server/json_mux"
 	"github.com/XJIeI5/card_game/pkg/socket/server/users"
 	tcp_server "github.com/sylpheeed/go-tcp-socket-chat"
 )
 
-func listen(u *users.User) {
-	data := json_mux.TemplateRequest{}
-	buf := make([]byte, 1024)
-	mux := helper.GetStandartJsonMux(func(err error) { u.RespondeError(err.Error()) })
-	for {
-		n, err := u.ReadConnection(buf)
-		if err != nil {
-			u.RespondeError("error: can't read from connection")
-		}
-		u.RespondeMessage(string(buf[:n]))
-		if err := json.Unmarshal(buf[:n], &data); err != nil {
-			u.RespondeError("undefiend responce")
-			u.Quit()
-		}
-
-		mux.ServeJson(u, data)
-	}
-}
-
 func main() {
 	serv := tcp_server.New("localhost:9999")
-	users.SetListenerForUsers(listen)
+	session := users.NewSession()
 	serv.OnNewClientCallback(func(conn net.Conn) {
-		users.CreateUser(conn)
+		handler := func(u *users.User, err error) { u.RespondeError(err.Error()) }
+		session.CreateUser(conn, handler)
 	})
 	serv.Listen()
 }
