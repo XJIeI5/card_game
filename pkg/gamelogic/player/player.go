f3ddf6e7ff19c5b1fb305c4a077ae25574847591
Егор Чернов <egorchernov37@yandex.ru>
2023-12-22 23:05:34 +0300

    cleared player logic

--- pkg/gamelogic/player/player.go
+++ pkg/gamelogic/player/player.go
@@ -1,8 +1,6 @@
 package player
 
 import (
-	"fmt"
-
 	"github.com/XJIeI5/card_game/pkg/gamelogic/card"
 )
 
@@ -10,53 +8,25 @@ type Player struct {
 	Name      string
 	Hand      card.Deck
 	creatures []*card.Creature
-	*SaveInput
-	*CardInput
+	SaveInput bool
+	CardInput *PlayCardConfig
+	err       error
 }
 
-func New(name string, cards []*card.Card) *Player {
+func New(name string, cards card.Deck) *Player {
 	player := &Player{
 		Name:      name,
-		Hand:      card.NewDeck(),
-		SaveInput: NewSaveInput(),
-		CardInput: NewCardInput(),
+		Hand:      cards,
+		SaveInput: false,
 	}
 	player.creatures = make([]*card.Creature, 0)
 	return player
 }
 
-func (p *Player) PlayCard() error {
-	if value, ok := <-p.CardInput.IsSet; !(value || ok) {
-		return fmt.Errorf("card input isn't set")
-	}
-	defer func() {
-		p.CardInput = NewCardInput()
-	}()
+func (p *Player) Error() error {
+	return p.err
+}
 
-	cardIndex := p.CardInput.PlayCardConfig.CardIndex
-	peekedCard := p.Hand[cardIndex]
-	if p.CardInput.PlayCardConfig.AsCreature {
-		p.creatures = append(p.creatures, card.NewCreature())
-		return p.Hand.Remove(cardIndex)
-	}
-
-	var prop *card.Property
-	if p.CardInput.PlayCardConfig.IsFirstProperty == nil {
-		return fmt.Errorf("is_first_property flag isn't set")
-	}
-
-	if *p.CardInput.PlayCardConfig.IsFirstProperty {
-		prop = &peekedCard.Property
-	} else {
-		prop = peekedCard.SecondProperty
-	}
-
-	if p.CardInput.PlayCardConfig.PeekedCreature == nil {
-		return fmt.Errorf("peeked_creature field isn't set")
-	}
-	err := p.Hand.Remove(cardIndex)
-	if err == nil {
-		err = p.creatures[*p.CardInput.PlayCardConfig.PeekedCreature].ApplyProperty(prop)
-	}
-	return err
+func (p *Player) AddError(err error) {
+	p.err = err
 }
