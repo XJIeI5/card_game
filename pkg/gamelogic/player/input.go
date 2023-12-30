3b53b28e4e80c539b5c7ce3b021717c5da99f38f
Егор Чернов <egorchernov37@yandex.ru>
2023-12-30 12:53:57 +0300

    added events

--- pkg/gamelogic/player/input.go
+++ pkg/gamelogic/player/input.go
@@ -1,8 +1,49 @@
 package player
 
+import (
+	"github.com/XJIeI5/card_game/pkg/gamelogic/card"
+	"github.com/nuttech/bell/v2"
+)
+
+const (
+	EventPlayCard   = "playcard"
+	EventPlayerPass = "playerpass"
+)
+
+var SharedEvents = BellState{bell.New()}
+
+type BellState struct {
+	*bell.Events
+}
+
+func (b *BellState) Reset() {
+	b.Events = bell.New()
+}
+
 type PlayCardConfig struct {
-	CardIndex       int
-	AsCreature      bool
-	IsUpperProperty *bool
-	PeekedCreature  *int
+	CardIndex      int
+	AsCreature     bool
+	Property       *card.Property
+	PeekedCreature *int
+}
+
+type PlayCardResult struct {
+	cfg    PlayCardConfig
+	player *Player
+}
+
+func (c *PlayCardResult) Player() *Player {
+	return c.player
+}
+
+func (c *PlayCardResult) Config() PlayCardConfig {
+	return c.cfg
+}
+
+type PassConfig struct {
+	player *Player
+}
+
+func (c *PassConfig) Player() *Player {
+	return c.player
 }
