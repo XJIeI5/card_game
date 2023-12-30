9f29b8e105e1eefa25f0c7f79d918a4bafb42423
Егор Чернов <egorchernov37@yandex.ru>
2023-12-30 12:53:01 +0300

    added log to error handler

--- cmd/server.go
+++ cmd/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net"
 
 	"github.com/XJIeI5/card_game/pkg/socket/server/users"
@@ -11,7 +12,10 @@ func main() {
 	serv := tcp_server.New("localhost:9999")
 	session := users.NewSession()
 	serv.OnNewClientCallback(func(conn net.Conn) {
-		handler := func(u *users.User, err error) { u.RespondeError(err.Error()) }
+		handler := func(u *users.User, err error) {
+			u.RespondeError(err.Error())
+			log.Print(u.Name(), err)
+		}
 		session.CreateUser(conn, handler)
 	})
 	serv.Listen()
