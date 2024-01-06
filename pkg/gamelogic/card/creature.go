decba58f5440ea6b9e9bee07d8c7ff5a4e718ab1
Егор Чернов <egorchernov37@yandex.ru>
2024-01-06 14:39:57 +0300

    ApplyProperty is working now, added new methods

--- pkg/gamelogic/card/creature.go
+++ pkg/gamelogic/card/creature.go
@@ -3,22 +3,45 @@ package card
 import "fmt"
 
 type Creature struct {
-	properties []*Property
-	foodAmount int
+	properties    []IProperty
+	foodAmount    int
+	maxFoodAmount int
 }
 
 func NewCreature() *Creature {
 	creature := &Creature{foodAmount: 1}
-	creature.properties = make([]*Property, 0)
+	creature.properties = make([]IProperty, 0)
 	return creature
 }
 
-func (c *Creature) ApplyProperty(prop *Property) error {
+func (c *Creature) ApplyProperty(prop IProperty) error {
+	samePropErr := fmt.Errorf("creature can't have two same properties")
+
 	for _, myProp := range c.properties {
-		if myProp.propertyType == prop.propertyType && !prop.canBeMultiple {
-			return fmt.Errorf("creature can't have two same properties")
+		if myProp == prop && !prop.CanBeMultiple() {
+			return samePropErr
 		}
 	}
+
+	if prop.LinkedCreature() != nil && prop.LinkedCreature().Has(prop) {
+		return samePropErr
+	}
+
 	c.properties = append(c.properties, prop)
+	prop.AssignToPair(prop.LinkedCreature())
 	return nil
+}
+
+func (c *Creature) Has(prop IProperty) bool {
+	for _, myProp := range c.properties {
+		if myProp == prop {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *Creature) Feed(foodAmount int) bool {
+
+	return false
 }
