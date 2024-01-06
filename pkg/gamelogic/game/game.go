538b2f8a7677a8129e5495539e2035ac75d98e87
Егор Чернов <egorchernov37@yandex.ru>
2024-01-06 14:41:22 +0300

    develop and food supply stages is now working

--- pkg/gamelogic/game/game.go
+++ pkg/gamelogic/game/game.go
@@ -3,20 +3,106 @@ package game
 import (
 	"log"
 	"sync"
+	"time"
 
 	"github.com/XJIeI5/card_game/pkg/gamelogic/player"
 	"github.com/nuttech/bell/v2"
 )
 
 type Game struct {
-	players player.Players
+	players []*player.Player
+	food    *FoodSupply
+	turn    turnManager
 
 	currPlayer int
 	mux        sync.Mutex
 }
 
 func New(players []*player.Player) *Game {
-	return &Game{players: players}
+	return &Game{players: players, turn: *newTurnManager()}
+}
+
+func (g *Game) Run() {
+	g.MakeTurn()
+	g.Run()
+}
+
+func (g *Game) MakeTurn() {
+	g.develop()
+	g.setFoodSupply()
+	// g.nutrition()
+	log.Print("end of turn")
+}
+
+func (g *Game) nutrition() {
+	defer g.resertEventSystem()
+
+	player.SharedEvents.Listen(player.EventFeedFromSupply, func(message bell.Message) {
+		// feedCfg := message.(player.FeedFromSupplyResult)
+
+	})
+
+	player.SharedEvents.Wait()
+}
+
+func (g *Game) setFoodSupply() {
+	g.food = newFoodSupply(len(g.players))
+}
+
+func (g *Game) develop() {
+	defer g.resertEventSystem()
+
+	var wg sync.WaitGroup
+	wg.Add(len(g.players))
+
+	player.SharedEvents.Listen(player.EventPlayCard, func(message bell.Message) {
+		g.playCardEventHandler(message)
+	})
+
+	player.SharedEvents.Listen(player.EventPlayerPass, func(message bell.Message) {
+		cfg := message.(player.PassResult)
+		time.Sleep(1 * time.Millisecond)
+		if g.canPass(message) {
+			log.Print(cfg.Player().Name, " passes")
+			g.turn.passFor(cfg.Player())
+			g.nextPlayer()
+			wg.Done()
+		}
+	})
+
+	player.SharedEvents.Wait()
+	wg.Wait()
+}
+
+func (g *Game) resertEventSystem() {
+	g.turn.reset()
+	player.SharedEvents.Reset()
+}
+
+func (g *Game) canPass(message bell.Message) bool {
+	passCfg := message.(player.PassResult)
+	if g.turn.isPassed(passCfg.Player()) {
+		return false
+	}
+	return g.CurrentPlayer() == passCfg.Player()
+}
+
+func (g *Game) playCardEventHandler(message bell.Message) {
+	pl := g.CurrentPlayer()
+
+	if g.turn.isPassed(pl) {
+		g.nextPlayer()
+		return
+	}
+
+	cfg := message.(player.PlayCardResult)
+	if cfg.Player() != pl {
+		return
+	}
+
+	log.Print(pl.Name, " plays card")
+	pl.PlayCard(cfg.Config())
+	g.nextPlayer()
 }
 
 func (g *Game) CurrentPlayer() *player.Player {
@@ -29,54 +115,12 @@ func (g *Game) nextPlayer() {
 	g.mux.Lock()
 	defer g.mux.Unlock()
 	g.currPlayer++
-	if g.currPlayer >= len(g.players) {
-		g.currPlayer = 0
-	}
-	log.Print("next, current ", g.currPlayer)
-}
-
-func (g *Game) Run() {
-	g.Step()
-	g.Run()
-}
-
-func (g *Game) Step() {
-	g.develop()
-	log.Print("end of turn")
-	g.players.Reset()
-	player.SharedEvents.Reset()
-}
-
-func (g *Game) develop() {
-	var myWg sync.WaitGroup
-	myWg.Add(len(g.players))
-
-	player.SharedEvents.Listen(player.EventPlayCard, g.playCardEventHandler)
-
-	player.SharedEvents.Listen(player.EventPlayerPass, func(message bell.Message) {
-		cfg := message.(player.PassConfig)
-		log.Print(cfg.Player().Name, " pass")
-
-		myWg.Done()
-	})
-
-	player.SharedEvents.Wait()
-	myWg.Wait()
-}
-
-func (g *Game) playCardEventHandler(message bell.Message) {
-	pl := g.CurrentPlayer()
-
-	if pl.IsPassed() {
-		g.nextPlayer()
+	g.currPlayer %= len(g.players)
+	if len(g.turn.passedPlayers) == len(g.players) {
 		return
 	}
-
-	cfg := message.(player.PlayCardResult)
-	if cfg.Player() != pl {
-		return
+	for g.turn.isPassed(g.players[g.currPlayer]) {
+		g.currPlayer++
+		g.currPlayer %= len(g.players)
 	}
-
-	log.Print(pl.Name, " play card")
-	g.nextPlayer()
 }
